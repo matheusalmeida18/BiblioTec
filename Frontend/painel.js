842087271d0328c98ce95fa96de06e443894abfc
Ieep.intranet <104377857+matheusalmeida18@users.noreply.github.com>
2026-03-29 23:34:48 -0300

    2 envio de info.

--- Frontend/painel.js
+++ Frontend/painel.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = 'https://bibliotec-ltu0.onrender.com/api';
 
 window.verificarAcesso = function() {
     const senha = document.getElementById('senhaAdmin').value.trim();
